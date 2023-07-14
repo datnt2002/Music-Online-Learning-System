2af485b12eae9b37498fb4f099a3a054f79df1d2
datnt2002 <103309042+datnt2002@users.noreply.github.com>
2023-07-14 16:45:24 +0700

    course to get you started 50%

---
+++ src/constants/mockData.js
@@ -0,0 +1,211 @@
+export const Courses = [
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 1,
+    title: 'Course Title',
+    price: '500,000',
+    image: '../assets/imgs/ngol.jpg',
+    author: 'Dat dep zai',
+    rating: '4.5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+];
+
+export const Categories = [
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+  {
+    id: 1,
+    title: 'Category',
+  },
+];
--- src/features/Home/Homepage.jsx
+++ src/features/Home/Homepage.jsx
@@ -3,6 +3,8 @@ import HeaderDefault from '../../components/Layout/User/HeaderDefault';
 import { Button, Card } from 'antd';
 import Meta from 'antd/es/card/Meta';
 import { Footer } from 'antd/es/layout/layout';
+import { Categories, Courses } from '../../constants/mockData';
+import { RightOutlined } from '@ant-design/icons';
 
 const Homepage = () => {
   return (
@@ -12,66 +14,88 @@ const Homepage = () => {
       <div
         className="bg-[#F39D39] h-16 rounded-full mt-4 ml-11 mr-11"
         style={{ boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' }}
-      ></div>
-
-      {/* course to get start */}
-      <div>
-        <h3>Courses to get you started</h3>
-        <Button>Most popular</Button>
-        <Button>Trending</Button>
-        <div>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-          </Card>
-        </div>
-      </div>
-
-      {/* Feature courses */}
-      <div>
-        <h3>Feature courses</h3>
-        <div>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-          </Card>
+      >
+        <div className="flex h-full content-center flex-wrap">
+          <div>
+            <h1>
+              Music <RightOutlined />
+            </h1>
+          </div>
+          {Categories.map((category, index) => {
+            return <p className=" flex-1">{category.title}</p>;
+          })}
         </div>
       </div>
 
       {/* course to get start */}
-      <div>
-        <h3>Courses to get you started</h3>
-        <Button>Most popular</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-        <Button>Trending</Button>
-      </div>
-
-      {/* Popular authors */}
-      <div>
-        <h3>Popular authors</h3>
+      <div className="ml-32 mr-32 mt-14">
         <div>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-          </Card>
+          <h6 className="mb-7">Courses to get you started</h6>
+          <Button className="mb-6">Most popular</Button>
+          <Button>Trending</Button>
+          <div className="flex">
+            {Courses.map((course, index) => {
+              return (
+                <Card hoverable cover={<img alt="example" src={course.image} />}>
+                  <Meta
+                    title={course.title}
+                    description={
+                      <>
+                        <h4>{course.author}</h4>
+                        <h3>{course.rating}</h3>
+                        <h1>{course.price}$</h1>
+                      </>
+                    }
+                  />
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+
+        {/* Feature courses */}
+        <div>
+          <h3>Feature courses</h3>
+          <div>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
+          </div>
+        </div>
+
+        {/* course to get start */}
+        <div>
+          <h3>Courses to get you started</h3>
+          <Button>Most popular</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+          <Button>Trending</Button>
+        </div>
+
+        {/* Popular authors */}
+        <div>
+          <h3>Popular authors</h3>
+          <div>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+              <Meta title="Europe Street beat" description="www.instagram.com" />
+            </Card>
+          </div>
         </div>
       </div>
       <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
