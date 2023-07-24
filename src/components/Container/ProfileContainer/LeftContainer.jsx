5b47aaff565892ea61928b7a721305dcca9781c1
datnt2002 <103309042+datnt2002@users.noreply.github.com>
2023-07-24 11:04:15 +0700

    update left container of profile

--- src/components/Container/ProfileContainer/LeftContainer.jsx
+++ src/components/Container/ProfileContainer/LeftContainer.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { AntDesignOutlined } from '@ant-design/icons';
 import { Avatar, Button } from 'antd';
-import { EditOutlined, EllipsisOutlined, SettingOutlined, ContactsOutlined } from '@ant-design/icons';
+import { EditOutlined, EnvironmentOutlined, ContactsOutlined, FacebookFilled } from '@ant-design/icons';
 import { Card } from 'antd';
 import { Link } from 'react-router-dom';
-
-const { Meta } = Card;
 
 const LeftContainer = () => {
   return (
@@ -21,13 +19,15 @@ const LeftContainer = () => {
 
         <h1 className="text-2xl font-semibold">Nguyen Trong Dat</h1>
         <p>Ke huy diet UI</p>
-        <p className="my-3">Ha Noi, Viet Nam</p>
+        <p className="my-3">
+          <EnvironmentOutlined className="align-[0.125rem]" /> Ha Noi, Viet Nam
+        </p>
 
         <Link>
           <Button
             type="primary"
             className="rounded-full w-full text-white font-bold text-base py-2 h-fit"
-            icon={<EditOutlined />}
+            icon={<EditOutlined className="align-[0.125rem]" />}
           >
             Edit your profile
           </Button>
@@ -36,34 +36,73 @@ const LeftContainer = () => {
         <Link>
           <Button
             type="primary"
-            className="rounded-full w-full text-white font-bold text-base py-2 h-fit my-4"
-            icon={<ContactsOutlined />}
+            className="rounded-full w-full text-white font-bold text-base py-2 h-fit my-4`"
+            icon={<ContactsOutlined className="align-[0.125rem]" />}
           >
             Teach on aleualeu
           </Button>
         </Link>
       </div>
 
-      <Card
-        style={{ width: 300 }}
-        cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-        actions={[<SettingOutlined key="setting" />, <EditOutlined key="edit" />, <EllipsisOutlined key="ellipsis" />]}
-      >
-        <Meta
-          avatar={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />}
-          title="Card title"
-          description="This is the description"
-        />
+      <Card size="small" title="About Dat dep zai" className="w-full mt-4">
+        <p>Card content</p>
+        <p>Card content</p>
       </Card>
 
-      <h2>Nguyen Trong Dat</h2>
-      <h2>Major</h2>
-      <h2>Location</h2>
+      {/* table statistic */}
+      <table className="w-full">
+        <tr>
+          <td>Project Views</td>
+          <td className="text-right">173</td>
+        </tr>
+        <tr>
+          <td>Appreciations</td>
+          <td className="text-right">173</td>
+        </tr>
+        <tr>
+          <td>Followers</td>
+          <td className="text-right">173</td>
+        </tr>
+        <tr>
+          <td>Following</td>
+          <td className="text-right">173</td>
+        </tr>
+      </table>
 
       {/* Social network */}
-      <h2>Nguyen Trong Dat</h2>
-      <h2>Major</h2>
-      <h2>Location</h2>
+      <div className="w-full">
+        <h2>On the web</h2>
+        <Link>
+          <Button
+            type="primary"
+            className="rounded-full w-full text-white font-bold text-base py-2 h-fit"
+            icon={<FacebookFilled className="align-[0.125rem]" />}
+          >
+            Facebook
+          </Button>
+        </Link>
+
+        <Link>
+          <Button
+            type="primary"
+            className="rounded-full w-full text-white font-bold text-base py-2 h-fit my-4`"
+            icon={<ContactsOutlined className="align-[0.125rem]" />}
+          >
+            Teach on aleualeu
+          </Button>
+        </Link>
+      </div>
+
+      {/* About me */}
+      <div className="max-w-[350px]">
+        <p>About me</p>
+        <p>
+          Xin chao! My name is Mai. I am a graphic designer and illustrator. I believe that graphic design is all about
+          having fun and exploring new perspectives, especially different culture and social knowledge.
+        </p>
+      </div>
+
+      <p>MEMBER SINCE: MAY 3, 2021</p>
     </div>
   );
 };
