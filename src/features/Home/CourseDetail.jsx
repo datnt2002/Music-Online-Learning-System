673de7b882e06e0ba9b1545fa2ad96f4ec2fad0b
Nguyễn Trọng Đạt <103309042+datnt2002@users.noreply.github.com>
2023-08-06 14:45:22 +0700

    update course content UI

--- src/features/Home/CourseDetail.jsx
+++ src/features/Home/CourseDetail.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Breadcrumb, Rate } from 'antd';
-import { ClockCircleOutlined, GlobalOutlined } from '@ant-design/icons';
+import {
+  ClockCircleOutlined,
+  CheckCircleOutlined,
+  GlobalOutlined,
+  CheckOutlined,
+  CaretRightOutlined,
+} from '@ant-design/icons';
 
 const CourseDetail = () => {
   return (
@@ -57,33 +63,105 @@ const CourseDetail = () => {
 
       {/* What you learn */}
       <div className="mx-auto max-w-7xl pt-11">
-        <div className='ml-16'>
-          <div>Show more pagraph</div>
+        <div className="ml-16 max-w-2xl">
+          {/* what you learn */}
+          <div className="border-2 border-yellow-300">
+            <h2 className="text-xl mx-6 mb-4"> What you learn in this course</h2>
+            <ul className="flex flex-wrap justify-between mx-6">
+              <li className="max-w-[18rem]">
+                <CheckOutlined /> HỌC đi đôi với "THỰC HÀNH", xây dựng ĐAM MÊ về lập trình với REACT
+              </li>
+              <li className="max-w-[18rem]">
+                <CheckOutlined /> HỌC đi đôi với "THỰC HÀNH", xây dựng ĐAM MÊ về lập trình với REACT
+              </li>
+              <li className="max-w-[18rem]">
+                <CheckOutlined /> HỌC đi đôi với "THỰC HÀNH", xây dựng ĐAM MÊ về lập trình với REACT
+              </li>
+              <li className="max-w-[18rem]">
+                <CheckOutlined /> HỌC đi đôi với "THỰC HÀNH", xây dựng ĐAM MÊ về lập trình với REACT
+              </li>
+            </ul>
+          </div>
+
           {/* course content */}
           <div>
-            <h1>Course Content</h1>
-            <h4>31 sections • 411 lectures • 67h 10m total length</h4>
+            <h2 className="text-xl mb-4">Course Content</h2>
+            <div className="flex justify-between">
+              <div className="flex">
+                <p>31 sections</p>
+                <CaretRightOutlined />
+                <p>411 lectures</p>
+                <CaretRightOutlined />
+                <p>67h 10m total length</p>
+              </div>
+              <button>Expand All Lessons</button>
+            </div>
             <div>List course dropdown</div>
           </div>
 
           {/* requirement */}
           <div>
-            <h1>Requirements</h1>
-            <p>doan nay o trong form dang ki </p>
+            <h2 className="text-xl mb-4">Requirements</h2>
+            <ul>
+              <li>
+                <CheckCircleOutlined />
+                Có tư duy lập trình
+              </li>
+              <li>
+                <CheckCircleOutlined />
+                Có tư duy lập trình
+              </li>
+              <li>
+                <CheckCircleOutlined />
+                Có tư duy lập trình
+              </li>
+            </ul>
           </div>
 
           {/* description */}
-          <div>Description</div>
+          <div>
+            <h2 className="text-xl mb-4">Description</h2>
+            <p>
+              React.JS là một thư viện, framework giúp xây dựng một website hiện đại, có tính mở rộng và hiệu năng cực
+              lớn. Các sản phẩm tiêu biểu sử dụng React có thể kể đến như Facebook và Instagram. Được Facebook chống
+              lưng, cũng như đầu tư mạnh mẽ, cộng với một cộng đồng đông đảo sử dụng, React chính là thư viện Frontend
+              phổ biến nhất hiện nay, bỏ xa Vue và Angular. Với tên gọi React ULTIMATE - mục tiêu đề ra của khóa học,
+              đấy chính là nó là phiên bản cuối cùng, là thứ DUY NHẤT các bạn cần, cũng như cập nhật MỚI NHẤT & ĐẦY ĐỦ
+              NHẤT cho người mới bắt đầu, muốn có một góc nhìn "thực sự chính xác" về React.JS. Ngoài ra, khi kết thúc
+              khóa học, các bạn mới bắt đầu sẽ có đủ tự tin để làm chủ React, cũng như hiểu được, nắm vững được những
+              kiến thức cốt lõi nhất để có thể xây dựng, phát triển một website thực tế với React.JS Khóa học sẽ thực sự
+              bổ ích cũng như mang lại rất nhiều kiến thức cho các bạn mới bắt đầu. Với phương châm, học đi đôi với thực
+              hành, chúng ta chỉ học "vừa đủ", chỉ học những kiến thức code lỗi nhất, hi vọng các bạn sẽ học hỏi được
+              nhiều kiến thức, cũng như tự tin sử dụng React cho công việc của mình.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="text-xl mb-4">Subcribes</h2>
+
+            <ul>
+              <li>
+                <CheckCircleOutlined />
+                Tất cả những bạn mới bắt đầu, những bạn muốn học lập trình với React, đang muốn học và làm chủ React thì
+                đây chính là khóa học các bạn tìm. React Ultimate, chỉ một là đủ.
+              </li>
+            </ul>
+          </div>
 
           {/* Best seller */}
-          <div></div>
+          <div>
+            <h2 className="text-xl mb-4">Best seller</h2>
+          </div>
 
           {/* recommend combo bought */}
-
-          <div></div>
+          <div>
+            <h2 className="text-xl mb-4">recommend combo bought</h2>
+          </div>
 
           {/* author */}
-          <div></div>
+          <div>
+            <h2 className="text-xl mb-4">Author</h2>
+          </div>
 
           {/* cmt and rating */}
           <div></div>
