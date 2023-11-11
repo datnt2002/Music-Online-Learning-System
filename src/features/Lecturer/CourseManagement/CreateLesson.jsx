7e08e2abcc14cdb28ed9fc7acc86a2e886123e68
Nguyễn Trọng Đạt <103309042+datnt2002@users.noreply.github.com>
2023-11-11 16:22:52 +0700

    create lesson form

--- src/features/Lecturer/CourseManagement/CreateLesson.jsx
+++ src/features/Lecturer/CourseManagement/CreateLesson.jsx
@@ -1,29 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
-import { Button, Form, Input, Select, Upload } from 'antd';
-import { InboxOutlined } from '@ant-design/icons';
+import { Radio } from 'antd';
 import { Content } from 'antd/es/layout/layout';
 
 import StepsCustom from '../../../components/Container/StepsContainer/StepsCustom';
 import BreadCrumbCustom from '../../../components/Container/BreadCrumbContainer/BreadCrumbCustom';
-import { CREATE_LESSON_FORM_FIELDS, VALIDATE_MESSAGE } from '../../../constants/formfield';
-import { createNewLessonAction, getDetailDraftCourseAction } from '../../../redux/slice/courseSlice';
+import { getDetailDraftCourseAction } from '../../../redux/slice/courseSlice';
 import { STORAGE } from '../../../constants';
 import repeatBg from '../../../assets/imgs/repeatbg.jpg';
 import Loading from '../../../components/Common/Loading';
+import CreateVideoLessonForm from '../../../components/Container/FormListContainer/CreateVideoLessonForm';
 
 const CreateLesson = () => {
+  const [typeOfLesson, setTypeOfLesson] = useState(true);
   const loading = useSelector((state) => state.course.loading);
-  const listSections = useSelector((state) => state.course.listSections);
-  const [file, setFile] = useState();
-  const [form] = Form.useForm();
   const { courseId } = JSON.parse(sessionStorage.getItem(STORAGE.COURSE));
-  console.log(courseId, listSections);
+  console.log(courseId);
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(
@@ -33,21 +28,10 @@ const CreateLesson = () => {
     );
   }, [dispatch, courseId]);
 
-  const handleSubmitLesson = (values) => {
-    console.log(values, file);
-    dispatch(
-      createNewLessonAction({
-        sectionId: values.sectionId,
-        lessonName: values.lessonName,
-        lessonDescription: values.lessonDescription,
-        file: file,
-        navigate,
-      })
-    );
-
-    form.resetFields();
+  const onChange = (e) => {
+    console.log(`radio checked:${e.target.value}`);
+    setTypeOfLesson(e.target.value);
   };
-
   return (
     <Content>
       {loading && <Loading />}
@@ -57,67 +41,14 @@ const CreateLesson = () => {
         </div>
         <StepsCustom step={2} />
         <div className="flex flex-col border border-black bg-white/50 rounded-2xl p-6">
-          <h1 className="font-semibold text-2xl mb-5">Create New Lesson</h1>
-          <Form form={form} name="create-lesson" onFinish={handleSubmitLesson}>
-            <Form.Item
-              label={CREATE_LESSON_FORM_FIELDS.SECTION_LABEL}
-              name={CREATE_LESSON_FORM_FIELDS.SECTION_ID}
-              rules={[{ required: true, message: VALIDATE_MESSAGE.SECTION_REQUIRED }]}
-            >
-              <Select className="border rounded-md">
-                {listSections.map((section) => {
-                  return (
-                    <Select.Option key={section.sectionId} value={section.sectionId}>
-                      {section.sectionName}
-                    </Select.Option>
-                  );
-                })}
-              </Select>
-            </Form.Item>
-            <Form.Item
-              name={CREATE_LESSON_FORM_FIELDS.LESSON_NAME}
-              label={CREATE_LESSON_FORM_FIELDS.LESSON_NAME_LABEL}
-              rules={[{ required: true, message: VALIDATE_MESSAGE.LESSON_REQUIRED }]}
-            >
-              <Input className="border rounded-md" />
-            </Form.Item>
-
-            <Form.Item
-              name={CREATE_LESSON_FORM_FIELDS.LESSON_DESCRIPTION}
-              label={CREATE_LESSON_FORM_FIELDS.LESSON_DESCRIPTION_LABEL}
-              rules={[{ required: true, message: VALIDATE_MESSAGE.LESSON_DESCRIPTION_REQUIRED }]}
-            >
-              <Input.TextArea rows={4} className="border rounded-md" />
-            </Form.Item>
-
-            <Form.Item label={CREATE_LESSON_FORM_FIELDS.LESSON_VIDEO_LABEL}>
-              <Form.Item
-                name={CREATE_LESSON_FORM_FIELDS.LESSON_VIDEO}
-                valuePropName="fileList"
-                getValueFromEvent={() => {}}
-              >
-                <Upload.Dragger
-                  name="files"
-                  maxCount={1}
-                  customRequest={(info) => {
-                    console.log(info);
-                    setFile(info.file);
-                  }}
-                >
-                  <p className="ant-upload-drag-icon">
-                    <InboxOutlined />
-                  </p>
-                  <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                  <p className="ant-upload-hint">Support for a single video.</p>
-                </Upload.Dragger>
-              </Form.Item>
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className="bg-black w-full">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+          <div className="flex justify-between">
+            <h1 className="font-semibold text-2xl mb-5">Create New Lesson</h1>
+            <Radio.Group onChange={onChange} defaultValue={typeOfLesson}>
+              <Radio.Button value={true}>Lesson</Radio.Button>
+              <Radio.Button value={false}>Exercise</Radio.Button>
+            </Radio.Group>
+          </div>
+          {typeOfLesson ? <CreateVideoLessonForm /> : <div></div>}
         </div>
       </div>
     </Content>
