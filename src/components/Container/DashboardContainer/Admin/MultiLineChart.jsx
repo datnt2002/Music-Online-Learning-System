1c2be2a7f3ec298d5bf1a21ec03f60039ddb388d
Nguyễn Trọng Đạt <103309042+datnt2002@users.noreply.github.com>
2023-11-13 22:44:22 +0700

    line chart

---
+++ src/components/Container/DashboardContainer/Admin/MultiLineChart.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const MultiLineChart = () => {
+  const data = {
+    labels: ['1', '1', '1', '1', '1', '1', '1'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+      },
+    ],
+  };
+  return (
+    <div className="col-span-12 rounded-xl border border-black bg-gray-100/50 px-5 pt-7.5 pb-5 shadow-2xl sm:px-7.5 xl:col-span-8">
+      <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
+        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
+          <div className="flex min-w-47.5">
+            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
+              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
+            </span>
+            <div className="w-full">
+              <p className="font-semibold text-primary">Total Revenue</p>
+              <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
+            </div>
+          </div>
+          <div className="flex min-w-47.5">
+            <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-secondary">
+              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-secondary"></span>
+            </span>
+            <div className="w-full">
+              <p className="font-semibold text-secondary">Total Sales</p>
+              <p className="text-sm font-medium">12.04.2022 - 12.05.2022</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="-ml-5">
+        <Line data={data} />
+      </div>
+    </div>
+  );
+};
+
+export default MultiLineChart;
