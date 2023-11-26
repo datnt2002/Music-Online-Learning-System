5aa02985da4a05a47de9f671badb8ab3e22ee190
Nguyễn Trọng Đạt <103309042+datnt2002@users.noreply.github.com>
2023-11-26 23:02:58 +0700

    css cart

--- src/features/Home/Cart.jsx
+++ src/features/Home/Cart.jsx
@@ -1,47 +1,47 @@
 import React from 'react';
 
 import { Button, Divider } from 'antd';
+
 import CardCart from '../../components/Container/Cart/CardCart';
+import repeatBg from '../../assets/imgs/repeatbg.jpg';
 
 const Cart = () => {
   return (
-    <div className="px-40 pt-10">
-      <h1 className="text-4xl font-semibold">Shopping Cart</h1>
-      <div className="flex">
-        <div className="flex flex-col flex-1">
-          <h1 className="my-4">1 Course in Cart</h1>
-          <Divider className="m-2" />
-          <div className="mr-10">
-            <CardCart />
+    <>
+      <Divider className="bg-black" />
+      <div
+        className="px-40 pt-10 min-h-screen"
+        style={{ backgroundImage: `url(${repeatBg})`, backgroundSize: '100% auto' }}
+      >
+        <h1 className="text-4xl font-semibold">Shopping Cart</h1>
+        <div className="flex">
+          <div className="flex flex-col flex-1">
+            <h1 className="my-4">1 Course in Cart</h1>
+            <Divider className="m-2" />
+            <div className="mr-10">
+              <CardCart />
+            </div>
           </div>
-        </div>
-        <div className="flex basis-1/3 ml-10">
-          <div class="flex flex-1 flex-col rounded-lg border p-6 shadow-xl">
-            <div class="mb-2 flex justify-between">
-              <p class="text-gray-700">Subtotal</p>
-              <p class="text-gray-700">$129.99</p>
-            </div>
-            <div class="flex justify-between">
-              <p class="text-gray-700">Shipping</p>
-              <p class="text-gray-700">$4.99</p>
-            </div>
-            <Divider />
-            <div class="flex justify-between">
-              <p class="text-lg font-bold">Total</p>
-              <div class="">
-                <p class="mb-1 text-lg font-bold">$134.98 USD</p>
-                <p class="text-sm text-gray-700">including VAT</p>
+          <div className="flex basis-1/3 ml-10">
+            <div class="flex flex-1 flex-col rounded-lg border p-6 shadow-xl">
+              <div class="mb-2 flex justify-between">
+                <p class="text-gray-700">Amount</p>
+                <p class="text-gray-700">$129.99</p>
               </div>
+              <Divider className="bg-black" />
+              <div class="flex justify-between">
+                <p class="text-lg font-bold">Total</p>
+                <div class="">
+                  <p class="mb-1 text-lg font-bold">134.98 ECoin</p>
+                  <p class="text-sm text-gray-700">including VAT</p>
+                </div>
+              </div>
+              <Button className="bg-black text-white mt-6 w-full rounded-md font-medium">Check out</Button>
             </div>
-            <Button className="mt-6 w-full rounded-md py-1.5 font-medium">Check out</Button>
           </div>
         </div>
       </div>
-
-      <div className="my-20">
-        <h1 className="text-4xl font-semibold">You might also like</h1>
-      </div>
-    </div>
+    </>
   );
 };
 
